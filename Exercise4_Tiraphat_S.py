ce921b44ed60753e6c47f9eb3ecbe0374e4bad73
Tian2531 <148762034+Tian2531@users.noreply.github.com>
2023-11-02 15:38:51 +0700

    Create Exercise4_Tiraphat_S.py

---
+++ Exercise4_Tiraphat_S.py
@@ -0,0 +1,15 @@
+# CP3_Tiraphat_Saeseai
+FE  = "Foundation English"
+GB  = "General Business"
+ICS = "Introduction to Computer Systems"
+CP  = "Computer Programming"
+
+print("--- Your Score ---")
+
+print(FE : 89)
+
+print(GB : 75)
+
+print(ICS : 80)
+
+print(CP : 78)
