70614e2fffa29581806836e6e73fa405b02a0bc3
Tian2531 <148762034+Tian2531@users.noreply.github.com>
2023-11-03 16:02:43 +0700

    Create Exercise5.1_Tiraphat_S.py

---
+++ Exercise5.1_Tiraphat_S.py
@@ -0,0 +1,8 @@
+# CP3_Tiraphat_Saeseai
+
+x = input()
+y = input()
+print(x,"+",y,"=",int(x)+int(y))
+print(x,"-",y,"=",int(x)-int(y))
+print(x,"*",y,"=",int(x)*int(y))
+print(x,"/",y,"=",int(x)/int(y))
