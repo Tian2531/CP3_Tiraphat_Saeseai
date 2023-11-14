61d994e2facfdbd5b73330f002398127d1824b9b
Tian2531 <148762034+Tian2531@users.noreply.github.com>
2023-11-14 16:42:47 +0700

    Create Lecture 50 Def.py

---
+++ Lecture 50 Def.py
@@ -0,0 +1,16 @@
+# CP3_Tiraphat_Saeseai
+#Def มีไว้สำหรับการประกาศฟังค์ชั่น
+#x,y คือ parameter (ช่องสำหรับใส่ข้อมูลลงไปตามรูปแบบที่เรากำหนด)
+def Addition_number(x,y):
+    print(x+y)
+def Subtraction_number(x,y):
+    print(x-y)
+def Multiplication_number(x,y):
+    print(x*y)
+def Division_number(x,y):
+    print(x/y)
+
+Addition_number(100,10)
+Subtraction_number(100,10)
+Multiplication_number(100,10)
+Division_number(100,10)
